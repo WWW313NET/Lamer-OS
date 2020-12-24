b005dca15e8a6b0af15c4e965899cf94985bd7e4
WWW313NET <75071180+WWW313NET@users.noreply.github.com>
2020-12-24 15:27:44 +0100

    Add files via upload

---
+++ shell.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+os.system("clear")
+try:
+	def _start_():
+  		print("Lamer OS")
+  		print("Version 2.0 by Kali Lincox")
+	def _init_():
+		cli = input("Lamer~Lincox~»»» ")
+		os.system(cli)
+		if (cli == "cd"):
+           		dir=input("enter a directory: ")
+           		os.chdir(dir)
+	_start_()
+
+	def lamer():
+    		while True:
+       			_init_()
+	lamer()
+except:
+  print("\nYou are a lamerottolo")
